68c015b51767c07a21db37a0aa5a37085f59de73
tone121 <tone121@cox.net>
2018-01-07 03:00:23 -0700

    src/kernel.c: fixed GPF crash fixed General Protection Fault crashes by keeping interrupts enabled during bootup.

--- src/kernel.c
+++ src/kernel.c
@@ -1,29 +1,17 @@
 #include <tty.h>
+#include <boot/idt.h>
 #include <boot/multiboot.h>
 #include <lib/string.h>
 #include <interrupt/pic.h>
 #include <mem/vmm.h>
 
 extern void gdt_init(void);
-extern void idt_init(void);
 
-void kernel_init(struct multiboot_info *mb, unsigned int mb_magic)
+void kernel_init()
 {
-	asm volatile ("cli"); /* disable interrupts during setup */
-
 	gdt_init();
 	idt_init();
 	pic_init();
-
-	tty_init(TTY_WHITE, TTY_BLACK);
-
-	asm volatile ("sti"); /* reenable them afterwards */
-
-	if (mb_magic != MULTIBOOT_MAGIC) {
-		tty_printf("Kernel not booted by compliant bootloader\n");
-		return;
-	}
-
 	vmm_init((void *)0x1000000);
 
 	return;

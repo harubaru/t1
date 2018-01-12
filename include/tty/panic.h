ba678c48579c6cb52e0aff6c6e041462217eee6c
tone121 <tone121@cox.net>
2018-01-12 07:52:21 -0700

    tty/panic: added a simple panic function added a simple panic function
    the other panic function was renamed to panic_dbg. while panic_dbg requires multiple arguments to print file and line, panic only requires one and that's to print the error message and make the kernel kill itself.

--- include/tty/panic.h
+++ include/tty/panic.h
@@ -3,8 +3,9 @@
 
 #include <tty/tty.h>
 
-#define PANIC(x) panic(__FILE__, __LINE__, x);
+#define PANIC(x) panic_dbg(__FILE__, __LINE__, x);
 
-void panic(char *file, unsigned int line, char *str);
+void panic(char *str);
+void panic_dbg(char *file, unsigned int line, char *str);
 
 #endif
--- src/tty/panic.c
+++ src/tty/panic.c
@@ -2,7 +2,13 @@
 
 extern void hang(void);
 
-void panic(char *file, unsigned int line, char *str)
+void panic(char *str)
+{
+	tty_printf("Kernel panic: %s\n", str);
+	hang();
+}
+
+void panic_dbg(char *file, unsigned int line, char *str)
 {
 	tty_printf("Kernel panic - %s @ %d: %s\n", file, line, str);
 	hang();

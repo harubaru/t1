e32719b91cad06321affb7fadd901b6690ff42c4
tone121 <tone121@cox.net>
2018-01-28 02:33:44 -0700

    mem/vmm: added more functions for versatility purposes

--- include/mem/vmm.h
+++ include/mem/vmm.h
@@ -4,9 +4,22 @@
 #include <mem/pmm.h>
 #include <stdint.h>
 
+typedef struct page_directory {
+	uint32_t *pd;
+	uint32_t *pt[1024];
+} page_directory_t;
+
+extern page_directory_t *kern_pd;
+extern page_directory_t *curr_pd;
+
 void vmm_init(void *end);
+page_directory_t *vmm_init_pd(void);
+void vmm_free_pd(page_directory_t *pd);
 void vmm_switch_pd(void *pd);
+void vmm_copy_pd(page_directory_t *dst, page_directory_t *src);
+void vmm_pd_free(page_directory_t *pd);
 void *vmm_map(void *addr);
+void *vmm_malloc(uint32_t size);
 void vmm_free(uint32_t *addr);
 
 #endif
--- src/mem/vmm.c
+++ src/mem/vmm.c
@@ -1,12 +1,8 @@
+#include <tty/tty.h>
 #include <mem/vmm.h>
 
 extern uint32_t __end_symbol;
 uint32_t end_symbol = (uint32_t)&__end_symbol;
-
-typedef struct page_directory {
-	uint32_t *pd;
-	uint32_t *pt[1024];
-} page_directory_t;
 
 page_directory_t *kern_pd = NULL;
 page_directory_t *curr_pd = NULL;
@@ -28,20 +24,34 @@ static void __enable_paging(void)
 	asm volatile("mov %eax, %cr0");	
 }
 
-static page_directory_t *init_pd(void)
+page_directory_t *vmm_init_pd(void)
 {
 	uint32_t i;
 	page_directory_t *pd = pmm_malloc(sizeof(page_directory_t));
 	pd->pd = pmm_malloc_a(1024 * sizeof(uint32_t));
 
 	for (i = 0; i < 1024; i++)
-		pd->pd[i] = 0x2;
+		pd->pd[i] = 0x3;
 
 	for (i = 0; i < 1024; i++)
 		pd->pt[i] = NULL;
 
 	return pd;
+}
 
+void vmm_free_pd(page_directory_t *pd)
+{
+	uint32_t i;
+
+	if (pd != curr_pd) {
+		// free page tables
+		for (i = 0; i < 1024; i++)
+			pmm_free(pd->pt[i]);
+		
+		// free page directory entries, then finally free the page directory structure.
+		pmm_free(pd->pd);
+		pmm_free(pd);
+	}
 }
 
 static void init_pt(uint32_t idx, page_directory_t *pd)
@@ -66,12 +76,11 @@ void vmm_init(void *end)
 
 	pmm_init(&__end_symbol, end);
 
-	kern_pd = init_pd();
+	kern_pd = vmm_init_pd();
 	curr_pd = kern_pd;
 
-	for (i = 0; i < 1024; i++) {
-		(void)vmm_map((void *)(i * 0x1000));
-	}
+	for (i = 0; i < (uint32_t)end; i++)
+		vmm_map((void *)(i));
 
 	vmm_switch_pd(kern_pd->pd);
 	__enable_paging();
@@ -81,6 +90,22 @@ void vmm_switch_pd(void *pd)
 {
 	curr_pd = pd;
 	__load_tlb((uint32_t)pd);
+}
+
+void vmm_copy_pd(page_directory_t *dst, page_directory_t *src)
+{
+	uint32_t i;
+	for (i = 0; i < 1024; i++) {
+		dst->pd[i] = src->pd[i];
+		dst->pt[i] = src->pt[i];
+	}
+}
+
+void vmm_pd_free(page_directory_t *pd)
+{
+	if ((pd) && (pd != kern_pd)) {
+		pmm_free(pd);
+	}
 }
 
 void *vmm_map(void *addr)
@@ -104,6 +129,14 @@ void *vmm_map(void *addr)
 	__flush_tlb();
 
 	return (void *)tmp;
+}
+
+void *vmm_malloc(uint32_t size)
+{
+	void *ret = pmm_malloc(size);
+	(void)vmm_map(ret);
+
+	return ret;
 }
 
 void vmm_free(uint32_t *addr)

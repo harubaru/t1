935a5bccd0b4edf03e9fcec49d6113b944ccdf8f
tone121 <tone121@cox.net>
2018-02-08 22:27:46 -0700

    src/boot/init.c: made boot screen nicer made boot screen nicer

--- src/boot/init.c
+++ src/boot/init.c
@@ -7,21 +7,32 @@
 #include <drivers/ps2/ps2kbd.h>
 #include <tty/tty.h>
 #include <mem/vmm.h>
+#include <sched/process.h>
 
 extern void __gdt_init(void);
 
-void init(struct multiboot_info *mb)
+#define OK tty_puts(" SUCCESS!\n", tty_default_bg(), TTY_LIGHT_GREEN);
+#define FAILED tty_puts(" FAILED!\n", tty_default_bg(), TTY_LIGHT_RED);
+
+void init_arch(struct multiboot_info *mb)
 {
-	tty_init(TTY_WHITE, TTY_BLACK);
-	vmm_init((void *)(mb->mem_upper + 2048));
+	tty_printf("Initializing arch specific code... ");
+
 	__gdt_init();
+	vmm_init((void *)(mb->mem_upper + 2048));
 	tss_init();
 	idt_init();
 	pic_init();
 	irq_init();
-	ps2kbd_init();
 
-	for(;;);
+	OK
+}
+
+void init(struct multiboot_info *mb)
+{
+	tty_init(TTY_WHITE, TTY_BLACK);
+	tty_printf("Kernel version %s\n", __KVERSION__);
+	init_arch(mb);
 
 	return;
 }
